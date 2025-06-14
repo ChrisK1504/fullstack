1dac572f8e98c4bfd972fac3e8161a37cef56f19
ChrisK1504 <krismehilli@yahoo.com>
2025-06-14 18:10:08 +0200

    Unicafe step 2

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -8,6 +8,18 @@ const App = () => {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+  const average = () => {
+    return (good - bad) / (good + neutral + bad)
+  }
+
+  const all = () => {
+    return good + neutral + bad
+  }
+
+  const positive = () => {
+    return good / all() * 100
+  }
+
   return (
     <div>
       <h1>give feedback</h1>
@@ -18,6 +30,9 @@ const App = () => {
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
+      <p>all {all()}</p>
+      <p>average {average()}</p>
+      <p>positive {positive()}%</p>
     </div>
   )
 }
