76d2449d4f48b234c510aa6ca22150159b8d2771
ChrisK1504 <krismehilli@yahoo.com>
2025-06-19 19:23:55 +0200

    Phonebook backend step 3

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -41,6 +41,17 @@ app.get('/info', (req, res) => {
     `)
 })
 
+app.get('/api/persons/:id', (req, res) => {
+  const id = req.params.id
+  const person = persons.find(p => p.id === id)
+
+  if (person) {
+    res.json(person)
+  } else {
+    res.status(404).end('Person not found')
+  }
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
